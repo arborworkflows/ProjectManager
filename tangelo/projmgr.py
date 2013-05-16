960f04534103cede0174466fa2074552b8c7a9d5
Roni Choudhury <roni.choudhury@kitware.com>
2013-05-16 13:30:55 -0400

    Added "get project list" action.

--- tangelo/projmgr.py
+++ tangelo/projmgr.py
@@ -1,8 +1,23 @@
 import tangelo
 
+from ArborFileManagerAPI import ArborFileManager
+api = ArborFileManager()
+api.initDatabaseConnection()
+
 @tangelo.restful
-def get(*pargs, **kwargs):
-    return "projmgr.get()"
+def get(*pargs):
+    if len(pargs) == 0:
+        return tangelo.HTTPStatusCode(400, "Missing resource type")
+
+    resource_type = pargs[0]
+    allowed = ["project"]
+    if resource_type == "project":
+        if len(pargs) > 1:
+            return tangelo.HTTPStatusCode(500, "Unimplemented - sorry!")
+        else:
+            return api.getListOfProjectNames()
+    else:
+        return tangelo.HTTPStatusCode(400, "Bad resource type '%s' - allowed types are: %s" % (resource_type, ", ".join(allowed)))
 
 @tangelo.restful
 def put(*pargs, **kwargs):
