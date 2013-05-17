d541058e08f448170fee928e3192a73554355b4b
Roni Choudhury <roni.choudhury@kitware.com>
2013-05-17 11:42:33 -0400

    Error message when database connection can't be made.

--- web-prototype/arborfilemanager.js
+++ web-prototype/arborfilemanager.js
@@ -5,6 +5,14 @@ window.onload = function () {
 
     ajax = d3.json("../tangelo/projmgr/project");
     ajax.send("GET", function (err, projects) {
+        if (err) {
+            d3.select(document.body)
+                .append("h1")
+                .style("color", "red")
+                .text("Could not connect to database");
+            return;
+        }
+
         d3.select("#projects")
             .selectAll("div")
             .data(projects)
