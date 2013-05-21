2ddf95eb6eb47d3a394ad0bb1eb793d54e155bb9
Roni Choudhury <roni.choudhury@kitware.com>
2013-05-21 18:00:07 -0400

    Flattened the control structure by creating independent "refresh" functions for the three columns of data.

--- web-prototype/arborfilemanager.js
+++ web-prototype/arborfilemanager.js
@@ -21,208 +21,103 @@ window.onload = function () {
         hl_type = null,
         hl_dataset = null;
 
-    function populate(projects){
+    function refreshProjects(){
         var ajax,
             items;
 
         ajax = d3.json(servicePath("project"));
-        ajax.send("GET", function (err, projects) {
-            if (err) {
-                error_message("Could not connect to database");
+        ajax.send("GET", function (e, projects) {
+            if (e) {
+                error_message("Error!");
+                console.log(e);
+                return false;
+            }
+
+            items = d3.select("#projects")
+                .selectAll("div")
+                .data(projects, function (d) {
+                    return d;
+                });
+
+            items.enter()
+                .append("div")
+                .classed("item", true)
+                .attr("name", function (d) {
+                    return d;
+                })
+                .text(function (d) {
+                    return d;
+                })
+                .on("click", function (d) {
+                    // Move the highlight to the selected project.
+                    if (hl_proj !== null) {
+                        d3.select("#projects")
+                            .select("[name=" + hl_proj + "]")
+                            .classed("selected", false);
+                    }
+                    hl_proj = d;
+                    d3.select(this)
+                        .classed("selected", true);
+
+                    // Refresh the data type list.
+                    refreshDatatypes(hl_proj);
+
+                    // Clear the data set list.
+                    clearDatasets();
+                });
+
+            items.exit()
+                .transition()
+                .duration(1000)
+                .style("height", "0px")
+                .style("opacity", 0.0)
+                .remove();
+
+            return true;
+        });
+    }
+
+    function refreshDatatypes(project) {
+        var ajax,
+            items;
+
+        ajax = d3.json(servicePath("project", project));
+        ajax.send("GET", function (e, datatypes) {
+            if (e) {
+                error_message("Error!");
+                console.log(e);
                 return;
             }
 
-        items = d3.select("#projects")
-            .selectAll("div")
-            .data(projects, function (d) {
-                return d;
-            });
-        items.enter()
-            .append("div")
-            .classed("item", true)
-            .on("click", function (d) {
-                var ajax,
-                    delbutton;
-
-                // If the click was on the highlighted item, bail.
-                //
-                // TODO(choudhury): really instead of bailing, this action
-                // should cause the page to *reload* all the information (in
-                // case it has been asynchronously changed by some other
-                // process).  In that case, we want to set a flag saying this is
-                // what we're doing, which would cause us to remember to keep
-                // any selected flag on the types, then initiate a click event
-                // on the selected type, then "cascade" this same process onto
-                // the datasets.
-                if (hl_proj && hl_proj.node() === this) {
-                    return;
-                }
-
-                // Move the highlight to the selected project.
-                if (hl_proj) {
-                    hl_proj.classed("selected", false);
-                }
-                hl_proj = d3.select(this);
-                hl_proj.classed("selected", true);
-
-                delbutton = d3.select(this)
-                    .select("a.btn")
-                    .attr("disabled", true);
-                delbutton.text("...");
-                delbutton.transition()
-                    .delay(1000)
-                    .text("..");
-                delbutton.transition()
-                    .delay(2000)
-                    .text(".");
-                delbutton.transition()
-                    .delay(3000)
-                    .text("delete")
-                    .attr("disabled", null);
-
-                ajax = d3.json(servicePath("project", d));
-                ajax.send("GET", function (err, types) {
-                    if (err) {
-                        console.log(err);
-                        error_message("Could not connect to database");
-                        return;
-                    }
-
-                    d3.select("#datatypes")
-                        .selectAll("div")
-                        .remove();
-
-                    d3.select("#datasets")
-                        .selectAll("div")
-                        .remove();
-
-                    d3.select("#datatypes")
-                        .selectAll("div")
-                        .data(types)
-                        .enter()
-                        .append("div")
-                        .classed("item", true)
-                        .on("click", function (d) {
-                            var ajax;
-
-                            // If the click was on the highlighted item, bail
-                            // (but, see TODO above).
-                            if (hl_type && hl_type.node() === this) {
-                                return;
-                            }
-
-                            // Move the highlight.
-                            if (hl_type) {
-                                hl_type.classed("selected", false);
-                            }
-                            hl_type = d3.select(this);
-                            hl_type.classed("selected", true);
-
-                            ajax = d3.json(servicePath("project", hl_proj.attr("name"), d));
-                            ajax.send("GET", function (err, datasets) {
-                                var dsets;
-
-                                if (err) {
-                                    error_message("Could not connect to database");
-                                    return;
-                                }
-
-                                d3.select("#datasets")
-                                    .selectAll("div")
-                                    .remove();
-
-                                dsets = d3.select("#datasets")
-                                    .selectAll("div")
-                                    .data(datasets)
-                                    .enter()
-                                    .append("div")
-                                    .classed("item", true)
-                                    .on("click", function (d) {
-                                        // If the click was on the highlighted
-                                        // item, bail (the TODO above doesn't
-                                        // apply in this case).
-                                        if (hl_dataset && hl_dataset.node() === this) {
-                                            return;
-                                        }
-
-                                        // Move the highlight.
-                                        if (hl_dataset) {
-                                            hl_dataset.classed("selected", false);
-                                        }
-                                        hl_dataset = d3.select(this);
-                                        hl_dataset.classed("selected", true);
-                                    })
-                                    .attr("name", function (d) {
-                                        return d;
-                                    })
-                                    .html(function (d) {
-                                        return d;
-                                    });
-
-                                    dsets.append("a")
-                                        .classed("btn", true)
-                                        .classed("btn-mini", true)
-                                        .text("preview");
-
-                                    dsets.append("a")
-                                        .classed("btn", true)
-                                        .classed("btn-mini", true)
-                                        .text("select");
-
-                                    dsets.append("a")
-                                        .classed("btn", true)
-                                        .classed("btn-mini", true)
-                                        .text("delete")
-                                        .on("click", function () {
-                                            var ajax;
-
-                                            ajax = d3.xhr(servicePath("project", hl_proj.attr("name"), hl_type.attr("name"), hl_dataset.attr("name")));
-                                            ajax.send("DELETE", function (e, r) {
-                                                if (e) {
-                                                    console.log(e);
-                                                    return;
-                                                }
-
-                                                console.log(r);
-                                            });
-                                        });
-                            });
-                        })
-                        .attr("name", function (d) {
-                            return d;
-                        })
-                        .text(function (d) {
-                            return d;
-                        });
+            items = d3.select("#datatypes")
+                .selectAll("div")
+                .data(datatypes, function (d) {
+                    return d;
                 });
 
-            })
-            .attr("name", function (d) {
-                return d;
-            })
-            .text(function (d) {
-                return d;
-            })
-            .append("a")
-            .classed("btn", true)
-            .classed("btn-mini", true)
-            .on("click", function () {
-                var ajax,
-                    name;
-
-                name = d3.select(this.parentNode).attr("name");
-                console.log(name);
-
-                ajax = d3.xhr(servicePath("project", name));
-                ajax.send("DELETE", function (e, r) {
-                    if (e) {
-                        console.log(e);
-                        return;
+            items.enter()
+                .append("div")
+                .classed("item", true)
+                .attr("name", function (d) {
+                    return d;
+                })
+                .text(function (d) {
+                    return d;
+                })
+                .on("click", function (d) {
+                    // Move the highlight to the selected datatype.
+                    if (hl_type !== null) {
+                        d3.select("#datatypes")
+                            .select("[name=" + hl_type + "]")
+                            .classed("selected", false);
                     }
+                    hl_type = d;
+                    d3.select(this)
+                        .classed("selected", true);
 
-                    populate();
+                    // Refresh the data set list.
+                    refreshDatasets(hl_proj, hl_type);
                 });
-            });
 
             items.exit()
                 .transition()
@@ -231,6 +126,62 @@ window.onload = function () {
                 .style("opacity", 0.0)
                 .remove();
         });
+    }
+
+    function refreshDatasets(project, type) {
+        var ajax,
+            items;
+
+        ajax = d3.json(servicePath("project", project, type));
+        ajax.send("GET", function (e, datasets) {
+            if (e) {
+                error_message("Error!");
+                console.log(e);
+                return;
+            }
+
+            items = d3.select("#datasets")
+                .selectAll("div")
+                .data(datasets, function (d) {
+                    return d;
+                });
+
+            items.enter()
+                .append("div")
+                .classed("item", true)
+                .attr("name", function (d) {
+                    return d;
+                })
+                .text(function (d) {
+                    return d;
+                })
+                .on("click", function (d) {
+                    // Move the highlight to the selected dataset.
+                    if (hl_dataset !== null) {
+                        d3.select("#datasets")
+                            .select("[name=" + d + "]")
+                            .classed("selected", false);
+                    }
+                    hl_dataset = d;
+                    d3.select(this)
+                        .classed("selected", true);
+                });
+
+            items.exit()
+                .transition()
+                .duration(1000)
+                .style("height", "0px")
+                .style("opacity", 0.0)
+                .remove();
+        });
+    }
+
+    function clearDatasets() {
+        d3.select("#datasets")
+            .selectAll("*")
+            .remove();
+
+        hl_dataset = null;
     }
 
     d3.select("#newproject-ok")
@@ -249,10 +200,10 @@ window.onload = function () {
                         return;
                     }
 
-                    populate();
+                    refreshProjects();
                 });
             }
         });
 
-    populate();
+    refreshProjects();
 };
