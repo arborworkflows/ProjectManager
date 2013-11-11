5cf5447b322cb2759f89a0da3dac80d3441f9971
Xiaoxiao Liu <xiaoxiaoliu@swk.local>
2013-11-10 22:24:10 -0500

    Fixed a bug in previous commit.
    Some of the internal tree nodes do not have names.
    Tested with anolis tree.

--- tangelo/phyloexport_algorithm.py
+++ tangelo/phyloexport_algorithm.py
@@ -1,38 +1,41 @@
 # -*- coding: utf-8 -*-
-
-from Bio import Phylo
-from Bio.Phylo import BaseTree
-
 import pymongo
-import logging
-from bson.objectid import ObjectId
-
 
 def recursive_build_newick(tree_coll, doc_id):
     nodeDoc = tree_coll.find_one({'_id':doc_id})
-    clades = nodeDoc['clades'] # a list of doc ids
+    clades = nodeDoc[u'clades'] # a list of doc ids
     newickString = "("
     for clade in clades:
       childDoc = tree_coll.find_one({'_id':clade})
-      nodeName = childDoc[u'name'] 
-      branchLength = childDoc[u'branch_length']
-      if childDoc.has_key('clades'):
+
+      if childDoc.has_key(u'clades'):
         subNewickString = recursive_build_newick(tree_coll,clade)
         newickString += subNewickString
-      newickString += nodeName + ":%f" %(branchLength)
+
+      if childDoc.has_key(u'name'):
+        nodeName = childDoc[u'name']
+        newickString += nodeName
+
+      if childDoc.has_key(u'branch_length'):
+        branchLength = childDoc[u'branch_length']
+        newickString += ":%f" %(branchLength)
+      else:
+        print "Warning: this node has empty branch length"
+
       newickString += ","
 
     # remove the last "," and replace it with ")"
-    newickString = newickString[:-1] + ") "
+    newickString = newickString[:-1] + ")"
     return newickString
 
 def convertTreeToNewickString(tree_coll):
     phylo = tree_coll.find_one({'rooted':True})
-    if phylo:
-       doc_id = phylo['clades'][0]#point to the acutally root
+    string = ""
+    if (phylo is not None):
+       doc_id = phylo[u'clades'][0]#point to the acutally root
        # newick string ends with ";"
-       newickString = recursive_build_newick(tree_coll, doc_id) + ";"
+       string = recursive_build_newick(tree_coll, doc_id) + ";"
     else:
         print 'failed to find the root document in the tree collection'
 
-    return newickString
+    return string
